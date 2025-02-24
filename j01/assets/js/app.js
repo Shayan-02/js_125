2a3fd7203d9b2640de1ed277b4fc914dbbea29ac
Shayan Shahbazi <upm200@gmail.com>
2025-02-24 14:17:48 +0330

    j01

---
+++ j01/assets/js/app.js
@@ -0,0 +1,5 @@
+let fname = "ali";
+let lname = "khodaee";
+let age = 14;
+
+console.log("your firstname is : \n", fname, "\nyour lastname is : \n", lname, "\nyour age is : \n", age);
---
+++ j01/assets/js/script.js
@@ -0,0 +1,33 @@
+// x = 5;
+// var x = 5;
+// var x = 'ali'
+// let x = 5;
+// x = 'ali';
+// const x = 5;
+// x = 10;
+
+let x = 10;
+let y = 20;
+
+let a = "ali ";
+let b = "reza"
+
+let g = a + b
+console.log(g);
+
+
+let c = 5
+let d = "ali"
+let u = d + c
+console.log(u);
+
+
+let z = x + y
+console.log(z);
+
+let p = "10";
+let q = 20;
+let w = p * q
+console.log(w);
+
+console.log(10, 20, "ali", true, w);
