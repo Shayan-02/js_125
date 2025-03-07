26fb3e4830e3c3703ba0dbd4cc502442ae21b219
Shayan Shahbazi <upm200@gmail.com>
2025-03-07 13:14:14 +0330

    Update script.js

--- j04/assets/js/script.js
+++ j04/assets/js/script.js
@@ -2,7 +2,7 @@ let user1 = prompt("انتخاب علی");
 let user2 = prompt("انتخاب ممد");
 
 if (user1 == "سنگ" || user1 == "کاغذ" || user1 == "قیچی"){
-    if (user1 == "سنگ" || user1 == "کاغذ" || user1 == "قیچی") {
+    if (user2 == "سنگ" || user2 == "کاغذ" || user2 == "قیچی") {
         if (user1 == user2){
             console.log("مساوی");
         }
